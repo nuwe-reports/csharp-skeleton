7024c8dbd34443a5272544d3e65f8e0c85e9b6af
Andreu Nuwe <andreu@nuwe.io>
2023-03-02 16:21:52 +0100

    Build inside .sh

--- .github/workflows/sonarqube.sh
+++ .github/workflows/sonarqube.sh
@@ -6,10 +6,8 @@ dotnet sonarscanner begin                                             \
   /d:sonar.login="${SONAR_TOKEN}"                                     \
   /d:sonar.coverageReportPaths="CoverageReport/SonarQube.xml"         \
   /d:sonar.dotnet.excludeTestProjects=true                            ;
-  
-dotnet restore                                                        ;
 
-dotnet build -c Release --no-restore                                  ;
+dotnet build --configuration Release                                  ;
 
 dotnet test --collect:"XPlat Code Coverage" -l:trx                    ;
 
@@ -17,9 +15,5 @@ reportgenerator                                                       \
   -reports:./test/**/coverage.cobertura.xml                           \
   -targetdir:./CoverageReport                                         \
   -reporttypes:'TextSummary;lcov;SonarQube'                           ;
-  
-dotnet publish src/WebApi/*.csproj                                    \
-  -c Release                                                          \
-  -o /app/publish --no-build --no-restore                             ;
-  
+
 dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"               ;
